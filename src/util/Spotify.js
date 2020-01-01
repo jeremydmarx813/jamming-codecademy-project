9abf85ecc4e89f17f535e1ed9f04817be4cbb95d
jeremydmarx <jeremydmarx@gmail.com>
2020-01-01 01:06:23 -0600

    completed save playlist, functionality not working

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -42,6 +42,38 @@ const Spotify = {
              };
          });
       });
+   },
+   savePlaylist(name, trackUris){
+        if(!name || !trackUris.length){
+            return;
+        }
+        const accessToken = Spotify.getAccessToken();
+        const headers = {
+           Authorization: `Bearer ${accessToken}`
+        };
+        let userId;
+        return fetch('https://api.spotify.com/v1/me', {headers: headers})
+        .then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            userId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, 
+               {
+                   headers: headers,
+                   method: 'POST',
+                   body: JSON.stringify({ 
+                       name: name 
+                   })
+               }).then(response => response.json())
+               .then(jsonResponse => {
+                   const playlistID = jsonResponse.id;
+                   return fetch(`/v1/users/${userId}/playlists/${playlistID}/tracks`, {
+                       headers: headers,
+                       method: 'POST',
+                       body: JSON.stringify({ URIs: trackUris })
+                   });
+               });
+        });
    }
 };
 
