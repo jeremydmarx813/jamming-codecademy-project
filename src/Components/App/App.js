d48c2b3ef0ec7c9892a9738968919676e03d21f7
jeremydmarx <jeremydmarx@gmail.com>
2021-01-08 01:18:20 -0600

    added conditional rendering to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,10 +7,12 @@ import Playlist from '../Playlist/Playlist';
 import Header from '../Header/Header';
 import LoginPage from '../Login/LoginPage';
 // import Spotify from '../../util/Spotify';
+import {useAuth0} from '@auth0/auth0-react';
 
 import { ContextProvider } from '../ContextProvider/ContextProvider';
 
 const App = () => {
+	const { isAuthenticated } = useAuth0()
 	return (
 		<ContextProvider>
 			<div>
@@ -18,13 +20,16 @@ const App = () => {
 
 				<div className="App">
 
-					<LoginPage />
-					{/* <SearchBar />
+					{!isAuthenticated ? <LoginPage /> :
+					<>
+					<SearchBar />
 
 					<div className="App-playlist">
 						<SearchResults />
 						<Playlist />
-					</div> */}
+					</div> 
+					</>
+					}
 				</div>
 			</div>
 		</ContextProvider>
