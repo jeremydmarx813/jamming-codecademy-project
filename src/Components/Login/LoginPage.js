ec7fa15c5f4226c748947b877b21462ec862a7e8
jeremydmarx <jeremydmarx@gmail.com>
2021-02-14 02:47:12 -0600

    prepped protec route on login

--- src/Components/Login/LoginPage.js
+++ src/Components/Login/LoginPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react';
+import { Route } from 'react-router-dom';
 
 
 function LoginPage() {
-    const { loginWithRedirect } = useAuth0();
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
     const styleObj = {
         display: 'flex',
         justifyContent: 'center',
