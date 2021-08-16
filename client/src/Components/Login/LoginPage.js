4ea4014ba94cf8bef814af2467cb30c9f7cd2a9c
jeremydmarx <jeremydmarx@gmail.com>
2021-08-16 15:59:51 -0500

    test for produciton

--- client/src/Components/Login/LoginPage.js
+++ client/src/Components/Login/LoginPage.js
@@ -6,7 +6,7 @@ function LoginPage() {
     const clientId = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_CLIENT_ID : process.env.CLIENT_ID;
     const redirectURI = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : process.env.REDIRECT_URI;
     const spotifyAuthLink = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectURI}&scope=playlist-modify-public&show_dialog=true`;
-
+    // console.log({clientId, redirectURI, spotifyAuthLink})
     return  (
         <div className="login-div">
             <button className="pill-button login-pill">
