2f0eb251b46dec0a1c5d9eda45348eb8145de033
jeremydmarx <jeremydmarx@gmail.com>
2021-08-17 16:33:28 -0500

    rebuild build

---
+++ client/build/precache-manifest.5de2750fe610c46dc03baf8364b3474b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35456637456a1db564de987f1dddaff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec283d6d036ca3b8af54",
+    "url": "/static/css/main.86206616.chunk.css"
+  },
+  {
+    "revision": "8df1aa83cbe9b786ae19",
+    "url": "/static/js/2.2229c31b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2229c31b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec283d6d036ca3b8af54",
+    "url": "/static/js/main.a2177f1e.chunk.js"
+  },
+  {
+    "revision": "dbb280643c154611f809",
+    "url": "/static/js/runtime-main.9a15b558.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
