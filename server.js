42fa13ed2b04ad8f1edce6b8833ca06dd8c5cc15
jeremydmarx <jeremydmarx@gmail.com>
2021-08-17 15:39:21 -0500

    changed server

--- server.js
+++ server.js
@@ -61,13 +61,14 @@ app.post("/login", (req, res) => {
 
 if (mode === "production") {
   console.log("production code block");
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
-  );
+  app.use(express.static('client/build'));
+  // app.use(express.static(path.resolve(__dirname, "./client/build")));
+  // app.get("*", (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+  // );
 } else {
   console.log("dev code block test");
-  morgan("dev");
+  app.use(morgan("dev"));
 }
 
 app.listen(port, () => {
