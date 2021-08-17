194b1c0c071315475e0d62a498b7d47fbaccbd66
jeremydmarx <jeremydmarx@gmail.com>
2021-08-17 15:09:35 -0500

    adjusted server

--- client/src/Components/Login/LoginPage.js
+++ client/src/Components/Login/LoginPage.js
@@ -6,8 +6,8 @@ function LoginPage() {
     const clientId = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_CLIENT_ID : process.env.CLIENT_ID;
     const redirectURI = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : process.env.REDIRECT_URI;
     const spotifyAuthLink = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectURI}&scope=playlist-modify-public&show_dialog=true`;
-    // console.log({clientId, redirectURI, spotifyAuthLink})
-    // console.log(process.env.NODE_ENV)
+    console.log({clientId, redirectURI, spotifyAuthLink})
+    console.log(process.env.NODE_ENV)
     return  (
         <div className="login-div">
             <button className="pill-button login-pill">
--- server.js
+++ server.js
@@ -1,75 +1,75 @@
-const express = require('express')
-const cors = require('cors');
-const dotenv = require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
 const SpotifyWebApi = require("spotify-web-api-node");
-const path = require('path');
+const path = require("path");
 const mode = process.env.MODE;
 const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET
+const clientSecret = process.env.CLIENT_SECRET;
 const redirectUri = process.env.REDIRECT_URI;
+const morgan = require("morgan");
 const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 
-
-
 app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
+  const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId,
     clientSecret,
     refreshToken,
-  })
+  });
 
   spotifyApi
     .refreshAccessToken()
-    .then(data => {
+    .then((data) => {
       res.json({
         accessToken: data.body.accessToken,
         expiresIn: data.body.expiresIn,
-      })
+      });
     })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
 
-app.post('/login', (req, res) => {
-
-  const code = req.body.code
+app.post("/login", (req, res) => {
+  const code = req.body.code;
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-  })
+  });
 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then(data => {
+    .then((data) => {
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
         expiresIn: data.body.expires_in,
-      })
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.sendStatus(400)
-    })
-})
+      res.sendStatus(400);
+    });
+});
 
-// console.log('production code block')
-app.use(express.static(path.join(__dirname, './client/build')))
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './client/build', 'index.html')));
-// if(mode === 'production'){
-// } 
-// else {
-//   console.log('dev code block test')
-// }
+if (mode === "production") {
+  console.log("production code block");
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+  );
+} else {
+  console.log("dev code block test");
+  morgan("dev");
+}
 
 app.listen(port, () => {
   console.log(`app listening at port ${port} in ${mode} mode.`);
-})
+});
